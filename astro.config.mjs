7b6c579773babb043964952284c86539d9f66fa1
Samvel Petrosyan <loidnoir@gmail.com>
2022-08-21 11:15:22 +0400

    🌿 GitHub Workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://loidnoir.github.io',
+  base: '/my-website',
   integrations: [tailwind()],
   vite: {
     ssr: {
